97ea64c5b2cb9333d03e9eee67ae95b472ba3de2
Kedar Naik <32413641+Kedar5@users.noreply.github.com>
2018-10-07 20:00:55 +0530

    Update log.py
    Added comments

--- log.py
+++ log.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python2.7
-
+#import psycopg2 module into the python code
 import psycopg2
 
-
+# Returns most popular three articles of all time
 def popular_articles():
-
+    # Establish connection with database and execute the query passed to it
     my_db = psycopg2.connect(database="news")
     query1 = """SELECT * from article_popularity limit 3"""
     cur1 = my_db.cursor()
@@ -16,7 +16,7 @@ def popular_articles():
     my_db.close()
     cur1.close()
 
-
+# Returns most popular three authors of all time
 def popular_authors():
     my_db = psycopg2.connect(database="news")
     query2 = """select * from authors_popularity"""
@@ -28,8 +28,8 @@ def popular_authors():
         print "%s - %d views" % (data[0], data[1])
     my_db.close()
     cur2.close()
-
-
+   
+# Returns date(s) with more than 1% of GET request errors
 def largest_errors():
     my_db = psycopg2.connect(database="news")
     query3 = """select * from cond_error"""
@@ -42,7 +42,7 @@ def largest_errors():
     my_db.close()
     cur3.close()
 
-
+# Prints results for each query
 if __name__ == '__main__':
     print "------------------------------------------------"
     popular_articles()
